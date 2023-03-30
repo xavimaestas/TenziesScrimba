68192177bf28dfa0dc6feb1012e0fbc62226297f
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2023-03-30 08:41:39 -0600

    random numbers working
    randome numbers working

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
 import './App.css'
+import Die from './components/Die'
 
 function App() {
 
+const [dice, setDice] = useState(allNewDice())
+
+const value = Math.floor(Math.random(1)*6)
+
+  function allNewDice(){
+    const newArray = []
+    for(let i=0; i<10; i++){
+      newArray.push({
+        value: Math.floor(Math.random(1)*6), 
+        isHeld: false
+      })
+    }
+    return newArray
+  }
+
+  const elementValue = dice.map(die => <Die value={die.value}/>)
+
+  function theRoller(){
+    setDice(allNewDice())
+  }
+  
+
   return (
-    <div className="App">
+    
+    <div className='App'>
+
+      
       <main className="main">
-        <div className="inner_div"></div>
+          <div className="die_container"> 
+            {elementValue}
+          </div>
+          <button className="roll_dice" onClick={theRoller}>Roll me</button>
       </main>
     </div>
+    
   )
 }
 
---
+++ tenzies/src/components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props){
+    return (
+        <div className="die_box">
+            <p className="dice">{props.value}</p>
+        </div>
+    )
+}
